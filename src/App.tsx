1eaad9270d395ca085ae4fb5ed460337dee9332d
limaluca <lucaslimamesmo@gmail.com>
2020-10-12 16:07:16 -0300

    Making the front/landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.css'
+import './styles/pages/landing.css'
+import {FiArrowRight} from 'react-icons/fi'
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="page-landing">
+
+      <div className="content-wrapper">
+
+        <img src={logoImg} alt="Logo do Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+
+          <div className="location">
+            <strong>São Luís</strong>
+            <span>Maranhão</span>
+          </div>
+
+          <a href="/" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0,0,0,0.9)"/>
+          </a>
+
+        
+        </main>
+      </div>
+   
     </div>
   );
 }
