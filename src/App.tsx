1fd5a29fc9c4574fe48ac4c045c938bbae5a5963
limaluca <lucaslimamesmo@gmail.com>
2020-10-13 08:14:59 -0300

    Using yarn to download a extension from react

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
 import React from 'react';
 import './styles/global.css'
-import './styles/pages/landing.css'
-import {FiArrowRight} from 'react-icons/fi'
-import logoImg from './images/logo.svg'
+import Routes from './routes'
 
 function App() {
   return (
-
-    <div id="page-landing">
-
-      <div className="content-wrapper">
-
-        <img src={logoImg} alt="Logo do Happy"/>
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-
-          <div className="location">
-            <strong>São Luís</strong>
-            <span>Maranhão</span>
-          </div>
-
-          <a href="/" className="enter-app">
-            <FiArrowRight size={26} color="rgba(0,0,0,0.9)"/>
-          </a>
-
-        
-        </main>
-      </div>
-   
-    </div>
+    <Routes/>
   );
 }
 
